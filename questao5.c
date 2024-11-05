a4162f88fa089a47913c1c0f61d7aa10e10834a5
Lucas-Andrade-Projetos <lucas.andradeprojetos@gmail.com>
2024-11-05 14:44:52 -0300

    --questão 5 finalizada, falta ajuste na 4 e lista de ponteiros.

--- questao5.c
+++ questao5.c
@@ -1,54 +1,69 @@
-/* 
-Desenvolva um programa que reformate os parágrafos de um texto, garantindo que ne
-nhuma linha exceda 80 caracteres.
+/*
+Desenvolva um programa que reformate os parágrafos de um texto, garantindo que 
+nenhuma linha exceda 80 caracteres.
 
-//perguntar para o professor o que se usa para entrar textos grandes pq isso eu copiei da internet e não deu muito bom
-
-Para rodar o programa
+para rodar o programa escreva no terminal do vscode:
 gcc -std=c99 -Wall -g -lm questao5.c -o questao5.exe
-Get-Content input3.txt | ./questao5.exe 
-*/
+Get-Content input3.txt | ./questao5.exe
 
+essa questão foi o capeta pqp;
+
+*/
 #include <stdio.h>
 #include <string.h>
 
-#define NMAX 1000
-
+#define NMAX 1000 
 void refatura_texto(char texto[NMAX]);
 
 int main()
 {
     char texto[NMAX];
-    char buffer[256]; 
+    int pos = 0;
 
-    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-        buffer[strcspn(buffer, "\n")] = '\0';
-        strcat(texto, buffer);
-        strcat(texto, " ");
+    while (pos < NMAX ) 
+    {
+        //Aqui eu ensiro o texto em um vetor e substituo todos suas quebras de linha por espaços.
+        int ch = getchar();
+        //esse if verifica se ainda tem texto a ser digitado, caso fosse um programa o usuario pararia digitando control + z 
+        if (ch == EOF) break;
+        
+        if(ch != '\n') {
+            texto[pos] = (char)ch;
+        }else{
+            texto[pos] = ' ';
+        }
+        pos++;
     }
+
+    //caractere nulo
+    texto[pos] = '\0'; 
     
+
     refatura_texto(texto);
-    printf("%s", texto);    
+    printf("%s", texto);
+    
     return 0;
 }
 
+//essa função recebe um vetor de n caracteres e adiciona uma quebra de linha a cada 80 caracteres
+void refatura_texto(char texto[NMAX]){
 
-
-void refatura_texto(char texto[NMAX])
-{
-
+    //essa função conta o tamanho do vetor
     int len = strlen(texto);
     int contador = 80;
-
+    //enquanto contador < que tamanho do vetor, ou seja a função vai adicionando quebra de linhas até no contador passar o tamanho do vetor
     while (contador < len) {
-        int pos = contador;
-        while(texto[pos] != ' '){
-            pos--;
+        int pos2 = contador;
+        //enquanto posição não for um espaço vazio ou seja estiver no meio de uma palavra ele vai diminuindo, o que isso significa?
+        //significa que pos[80] estiver no meio de uma palavra ele diminui até achar um espaço vazio.
+        while(texto[pos2] != ' '){ 
+            pos2--;
         }
-
-        if(pos > 0){
-            texto[pos] = '\n';
-            contador = pos + 80;
+        //quando acha se pos > 0 ele adiciona a quebra de linha e contador recebe mais 80.
+        if(pos2 > 0){
+            texto[pos2] = '\n';
+            contador = pos2 + 80;
         }
     }
+
 }
